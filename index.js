7f7514083cc18de8995bcca116211d3f57da3e81
akisblack <mavrommatisakis@gmail.com>
2020-12-31 17:38:23 +0200

    Added commands, edited others
    troubleshooting loaders, converting drives to mbr, organising games, installing forwarders. edited credits to include more people

--- index.js
+++ index.js
@@ -49,13 +49,13 @@ bot.on("message", async message => {
 **/ios** - Displays a link with downloads to various useful Wii IOS files **FOR REGULAR WII**
 **/sysmenu** - Displays a link with downloads to 4.3 Wii System Menu files from all regions **FOR REGULAR WII**
 **/iosmini** - Displays a link with downloads to various useful Wii mini IOS files **FOR WII MINI**
+**/troubleshoot** - Displays 6 possible reasons your loader isn't working right away
+**/mbr** - Displays a way to convert a drive to the MBR partition table 
+**/wbm** - Shows a guide for organising your digital Wii game library
 **/sysmenumini** - Displays a link with downloads to the Wii mini System Menu files from all regions **FOR WII MINI**
+**/forwarders** - Installing forwarders for games
 **/vwii** - Displays the currently recommended vWii guide`);
    }
-
-   
-
-
 
     // Guide
     if(cmd === `${prefix}guide`) {
@@ -125,7 +125,14 @@ Use Bluebomb for the Wii mini and Letterbomb for the regular Wii`);
 
     // Credits
     if(cmd === `${prefix}credits`) {
-    return message.channel.send(`Bot creator: akisblack#2545`);
+    return message.channel.send(`Bot creator: 
+akisblack#2545
+
+Kudos: 
+shrek#7532 aka DerpMaster2 
+WiiMaster#7071 aka WiiExpertise 
+MethodOrMadness#9199 aka Method 
+and many others`);
     }
 
     // vWii
@@ -133,6 +140,11 @@ Use Bluebomb for the Wii mini and Letterbomb for the regular Wii`);
     return message.channel.send(`https://wiiu.hacks.guide/#/vwii-modding`);
     }
     
+    // Forwarders
+    if(cmd === `${prefix}forwarders`) {
+        return message.channel.send(`https://wii.guide/wiigsc`);
+        }
+
     // Root
     if(cmd === `${prefix}root`) {
     return message.channel.send(`https://imgur.com/a/HDCWvt0`);
@@ -162,6 +174,55 @@ Use Bluebomb for the Wii mini and Letterbomb for the regular Wii`);
     // System Menu Wii mini
     if(cmd === `${prefix}sysmenumini`) {
     return message.channel.send(`https://e.pcloud.link/publink/show?code=kZVzl7ZkUOu6Nzqu24zTH00cUCPq5vncQFk`);
+    } 
+
+    // Loader Troubleshooting
+    if(cmd === `${prefix}troubleshoot`) {
+    return message.channel.send(`1: Wrong USB port. The only USB port that can run game backups is the one on the edge of the Wii, unless you are using d2x beta53-alt cIOS, which is not recommended as it is not as compatible.
+
+2: cIOS is not set up correctly (follow https://wii.guide/cios). We can only confirm it is done right with a syscheck (follow https://wii.guide/syscheck). Keep in mind that some games that use accessories will require a different IOS to run.
+    
+3: Image is bad/corrupted. Testable in Dolphin Emulator. Make sure it's wbfs or iso (not nkit) if you're trying to play a Wii game.
+    
+4: Use the latest version of your loader, use default settings and name the games as such for Wii games> USB:/wbfs/GameName [GameID]/GameID.wbfs
+    
+5: USB isn't compatible. That is most likely to happen with flash drives. HDDs are always the best way to store games on the Wii.
+    
+6: Don't use a forwarder. Launch the app directly from the Homebrew Channel.`);
+    } 
+    
+    // MBR
+    if(cmd === `${prefix}mbr`) {
+    return message.channel.send(`To change a drive to MBR on Windows, open disk management. If you're on Windows 8 or higher, do Windows key + X and click on disk management.
+
+Once you're there, find the drive you need to convert to MBR and right click on the area where it says "Disk" and then the number of the disc. If it's not already MBR, you'll see an available option to convert to MBR disk. Use that option.`);
+    } 
+
+    // Organising Games
+    if(cmd === `${prefix}wbm`) {
+    return message.channel.send(`The best way to fix Wii Backup Manager not working is by deleting Wii Backup Manager and never looking back.
+
+There's a far quicker and more reliable method of adding games - and that's manually, by making folders and dropping your games in their correct folders.
+        
+Some people say it's hard, but it's nowhere near as hard as they make it out to be. Took me about 25 minutes to add roughly 30 games, Wii and GameCube. Though the amount of time it takes is largely dependent on what USB interface you're using (1.0, 1.1, 2.0, 3.0, 3.1) what kind of device you're using (hard drive, SD card, flash drive, SSD), and how large the files are.
+        
+The first step is making a "wbfs" folder on the root of your USB device if there isn't already one. If there already is one, then you're already one step ahead.
+        
+Inside the "wbfs" folder will be game folders, once you've made them. For example, inside that "wbfs" folder, Wii Sports would look like this:
+        
+/USB drive/wbfs/Wii Sports [RSPE01]/RSPE01.wbfs
+        
+If you're using .iso files instead of .wbfs files, make sure to change them using this tool here. It's a far superior way of storing games on external storage.
+        
+https://isotowbfs.com/
+        
+That 6 digit code after the name of the game is called the game ID. You can find them here by searching the game's name and copying that code.
+        
+https://gametdb.com/
+        
+After you have the game folder, which is inside the "wbfs" folder, plop your file inside. It must be named like the example above.
+        
+And congratulations, you just added a Wii game to your USB device manually. You'll get faster the more you do it.`);
     } 
 
 });
