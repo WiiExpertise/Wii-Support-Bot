102d8b6b2b415057b1df1dee06679e21ac81884d
akisblack <mavrommatisakis@gmail.com>
2021-01-03 08:34:10 +0200

    Update main.py

--- Python Version/main.py
+++ Python Version/main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 client = commands.Bot(command_prefix='/')
 
 #sends message to console that the client is online
-print('I am online')
+print('i am online')
 
 # sets activity
 @client.event
